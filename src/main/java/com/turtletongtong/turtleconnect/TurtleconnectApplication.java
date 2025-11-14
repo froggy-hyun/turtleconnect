9c55dda0ec3c2c65fa06b4ec7dbe04bb986abdf4
Evan Choi (최성현) <111727754+froggy-hyun@users.noreply.github.com>
2025-11-14 15:03:30 +0900

    init: spring boot

---
+++ src/main/java/com/turtletongtong/turtleconnect/TurtleconnectApplication.java
@@ -0,0 +1,13 @@
+package com.turtletongtong.turtleconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurtleconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurtleconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turtletongtong/turtleconnect/TurtleconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turtletongtong.turtleconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurtleconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
